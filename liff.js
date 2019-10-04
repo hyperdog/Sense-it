cf5e9736e1923f5e8d451b25053676c997e71c18
hyperdog <hyperdogclay@gmail.com>
2019-10-04 19:55:23 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -1,5 +1,5 @@
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID         = 'U0f9e99042f1d948f7da53a336fe8a409'; // LED, Button
+const USER_SERVICE_UUID         = '2afc99d1-091a-4789-8ff7-24d75101ba67'; // LED, Button
 // User service characteristics
 const LED_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 const BTN_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
